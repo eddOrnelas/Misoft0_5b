4b91f7dc72150aedfcefcc04a70842172513ddd6
Eddie Ornelas <numcomx@gmail.com>
2013-11-11 17:48:42 -0800

    Rollback Eddie

--- src/venta/ArticuloVenta.java
+++ src/venta/ArticuloVenta.java
@@ -2,10 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+//Comentario Eddie
 package venta;
 
 import java.sql.Date;
 import misoft.CustomORM;
+//Noe Muñoz
 
 
 /**
